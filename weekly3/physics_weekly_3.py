ff2742ee14a93b4057db28f85e148a8393a9a26f
Tatu Lehtola <tatu.lehtola1996@gmail.com>
2025-02-22 18:25:16 +0200

    Physics Project

--- weekly3/physics_weekly_3.py
+++ weekly3/physics_weekly_3.py
@@ -16,9 +16,9 @@ for i in range(len(latitudes)):
 
 fo.PolyLine(trail_coordinates, tooltip="Polku", color='red').add_to(m)
 
-m.save('linnanmaa.html')
+m.save('path.html')
 
-print("Map saved to linnanmaa.html")
+print("Map saved to path.html")
 
 def haversine(lat1, lon1, lat2, lon2):
     R = 6371
@@ -33,3 +33,42 @@ for i in range(len(latitudes) - 1):
     total_distance += haversine(latitudes[i], longitudes[i], latitudes[i + 1], longitudes[i + 1])
 
 print("Total distance: {:.2f} km".format(total_distance))
+
+
+
+"""
+ax[1].plot(time, acceleration_y, label='Y')
+plt.xlabel('Time (s)')
+plt.ylabel('Acceleration (m/s^2)')
+ax[0].grid()
+plt.legend()
+
+ax[2].plot(time, acceleration_z, label='Z')
+plt.xlabel('Time (s)')
+plt.ylabel('Acceleration (m/s^2)')
+ax[0].grid()
+plt.legend()
+"""
+
+"""
+ax[1].plot(time, y_filtered, label='Y')
+plt.xlabel('Time (s)')
+plt.ylabel('Acceleration (m/s^2)')
+ax[1].grid()
+plt.legend()
+
+ax[2].plot(time, z_filtered, label='Z')
+plt.xlabel('Time (s)')
+plt.ylabel('Acceleration (m/s^2)')
+ax[2].grid()
+plt.legend()
+"""
+
+"""
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.plot(time, acceleration_x, label='X')
+plt.xlabel('Time (s)')
+plt.ylabel('Acceleration (m/s^2)')
+ax.grid()
+st.pyplot(fig)
+"""
